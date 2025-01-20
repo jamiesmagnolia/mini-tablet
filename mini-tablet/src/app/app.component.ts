67d8c0c4c33b66648aba7c0bb4c6bd152068025e
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2025-01-21 01:16:43 +1100

    Add misc.

--- mini-tablet/src/app/app.component.ts
+++ mini-tablet/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, FooterComponent],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
