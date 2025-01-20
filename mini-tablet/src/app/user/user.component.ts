78aa4771c3dd05df7b5db563d42a5d284a808510
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2025-01-21 00:30:28 +1100

    Add misc.

--- mini-tablet/src/app/user/user.component.ts
+++ mini-tablet/src/app/user/user.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule, MatCardModule, MatButtonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
 
+  userName = '';
+  passWord = '';
+
+  // favoriteFramework = '';
+
+  // showFramework() {
+  //   alert(this.favoriteFramework);
+  // }
 }
