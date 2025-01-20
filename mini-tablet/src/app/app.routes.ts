4c803f6ec3449e80df1494e4ed0aa7c5d062980f
jamiesmagnolia <jamie.a.magnolia@gmail.com>
2025-01-20 21:08:16 +1100

    Add basic page routing

--- mini-tablet/src/app/app.routes.ts
+++ mini-tablet/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'App Home Page',
+        component: HomeComponent,
+    },
+    {
+        path: 'user',
+        component: UserComponent,
+    },
+];
